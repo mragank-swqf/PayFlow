d28ac021a0a63f7a74aeb55718595070f9d313e6
mragank-swqf <mragank001@gmail.com>
2026-06-22 15:16:58 +0530

    Connect Redis

---
+++ app/core/redis.py
@@ -0,0 +1,16 @@
+import redis.asyncio as aioredis
+from app.core.config import settings
+
+redis_client: aioredis.Redis | None = None
+
+async def get_redis() -> aioredis.Redis:
+    return redis_client
+
+async def connect_redis():
+    global redis_client
+    redis_client = aioredis.from_url(settings.REDIS_URL, decode_responses=True)
+
+async def disconnect_redis():
+    global redis_client
+    if redis_client:
+        await redis_client.aclose()
